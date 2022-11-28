01c864a91cec0b77fa458710e13f2e56bdeca694
Nicolas CASTANET <castanet@gpu.scai.sorbonne-universite.fr>
2022-11-28 17:39:59 +0100

    add GMazeGoal

--- gym_gmazes/envs/__init__.py
+++ gym_gmazes/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_gmazes.envs.gmaze_dubins import GMazeDubins, GMazeGoalDubins
+from gym_gmazes.envs.gmaze_dubins import GMazeDubins, GMazeGoalDubins, GMazeGoal
 from gym_gmazes.envs.maze.maze import get_maze

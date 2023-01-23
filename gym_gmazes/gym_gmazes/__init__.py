71a3f8ded991b5515aa2228e129c5ce43f52980f
Nicolas CASTANET <castanet@gpu.scai.sorbonne-universite.fr>
2023-01-23 11:21:50 +0100

    misc.

---
+++ gym_gmazes/gym_gmazes/__init__.py
@@ -0,0 +1,34 @@
+import os
+import gym
+from gym.envs.registration import register
+from gym_gmazes.envs.maze.maze import get_maze
+
+__version__ = "0.1.0"
+
+
+def envpath():
+    resdir = os.path.join(os.path.dirname(__file__))
+    return resdir
+
+
+print("gym-gmazes: ")
+print("|    gym version and path:", gym.__version__, gym.__path__)
+
+print("|    REGISTERING GMazeDubins-v0 from", envpath())
+register(
+    id="GMazeDubins-v0",
+    entry_point="gym_gmazes.envs:GMazeDubins",
+)
+
+print("|    REGISTERING GMazeGoalDubins-v0 from", envpath())
+register(
+    id="GMazeGoalDubins-v0",
+    entry_point="gym_gmazes.envs:GMazeGoalDubins",
+)
+
+
+print("|    REGISTERING GMazeGoal-v0 from", envpath())
+register(
+    id="GMazeGoal-v0",
+    entry_point="gym_gmazes.envs:GMazeGoal",
+)
---
+++ gym_gmazes/gym_gmazes/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_gmazes.envs.gmaze_dubins import GMazeDubins, GMazeGoalDubins, GMazeGoal
+from gym_gmazes.envs.maze.maze import get_maze
